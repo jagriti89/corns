cb3551801cddd6fede8346c4a39e3b01d12ec6c1
jagriti89 <jagritisinghpro@gmail.com>
2023-02-13 15:45:21 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-const express=require('express')
-const  mongoose=require('mongoose');
-const route=require("./route.js")
-const app=express();
-app.use(express.json())
-mongoose.set('strictQuery',true)
-mongoose.connect("mongodb+srv://jagriti:Jaggu123@cluster0.nf3nfa7.mongodb.net/corns",{
-    useNewUrlParser:true
-})
-.then(()=>console.log("mongoose.connect"))
-.catch(err=>console.log(err))
-app.use("/",route)
-app.listen(3000,function(){
-    console.log("running on"+3000)
-})
+const express=require("express")
+const app=express()
+
+const cron = require("node-cron");
+const router = express. Router();
+const route=require("./file")
+ app.use("/",route)
+
+ app.listen(process.env.PORT || 3000, function(){
+    console.log("running at "+(process.env.PORT||3000))
+ })
